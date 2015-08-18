c3e80bda388ff2b8005ea42a87e0402bc5b61e8b
Andre Almeida <andre.almeida@msn.com>
2015-08-18 13:43:03 -0300

    Andre | Otavio - Adicionando injeção de dependências

--- TW/exeCalculos/src/FormaGeometrica.java
+++ TW/exeCalculos/src/FormaGeometrica.java
@@ -1,15 +1,17 @@
 
 public class FormaGeometrica {
+    private Imprimir imprime;
 
-
-
+    public FormaGeometrica(Imprimir imprime) {
+        this.imprime = imprime;
+    }
 
     public void calculaFormas(Figura_abs formaGeometrica){
-        Imprimir imprime = new Imprimir();
 
         imprime.imprimeNaTela(  "Área do " + formaGeometrica.getNomeObjeto() +  " = "  , formaGeometrica.calculaArea());
         imprime.imprimeNaTela(  "Perimetro do " + formaGeometrica.getNomeObjeto() +  " = "  , formaGeometrica.calculaPerimetro());
         System.out.println("============================");
+
     }
 
 }
--- TW/exeCalculos/src/MAIN.java
+++ TW/exeCalculos/src/MAIN.java
@@ -4,11 +4,14 @@ public class MAIN {
     public static void main(String[] args) {
 
 
+        Imprimir imprime = new Imprimir();
+
         FormaGeometrica forma = new FormaGeometrica();
 
         forma.calculaFormas(new Quadrado(2,4));
         forma.calculaFormas(new Retangulo(2,6));
         forma.calculaFormas(new Losango(4,3));
+
 
 
 
